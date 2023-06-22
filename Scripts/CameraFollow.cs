7233ed927a45c0e42fb5dc2b9a569a594c60ac52
Tunjii10 <oluwatunji7@gmail.com>
2023-06-22 19:40:11 +0100

    camera-follow

---
+++ Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraFollow : MonoBehaviour
+{
+    public float mouseSensitivity = 2f;
+
+
+    void Start()
+    {
+        // lock and hide Cursor
+
+    }
+
+    void Update()
+    {
+        transform.eulerAngles += mouseSensitivity * new Vector3(x:-Input.GetAxis("Mouse Y"),y:Input.GetAxis("Mouse X"),z:0);
+       
+    }
+
+}
